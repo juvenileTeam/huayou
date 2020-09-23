4c66a54cdc298b76c2f56ca73beefe488aed5bb5
Dback_centos <634008415@qq.com>
2020-09-23 19:38:10 +0800

    添加登录验证中间件

---
+++ huayou/common/middleware.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from django.utils.deprecation import MiddlewareMixin
+
+
+class AuthMiddleware(MiddlewareMixin):
+    white_list = [
+        '/api/user/vcode/fetch/'
+        '/api/user/vcode/submit/'
+    ]
+
+    def process_request(self, request):
+        if request.path in self.white_list:
+            return
+        uid = request.session.get('uid')
+        if not uid:
+            return JsonResponse({'code': 1002, 'data': '用户未登录'})
--- huayou/huayou/settings.py
+++ huayou/huayou/settings.py
@@ -46,6 +46,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'common.middleware.AuthMiddleware'
 ]
 
 ROOT_URLCONF = 'huayou.urls'
